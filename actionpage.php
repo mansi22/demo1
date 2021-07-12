665b7cc9717c98942835829ac09a17c8877805af
mansi22 <mansithakkar9990@gmail.com>
2021-07-12 20:58:45 +0530

    Add files via upload

---
+++ actionpage.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+First Name :<?php echo $_POST["fname"]; ?><br>
+last Name: <?php echo $_POST["lname"]; ?>
+Country: <?php echo $_POST["country"]; ?>
+Language: <?php echo $_POST["fav_language"]; ?>
+Text: <?php echo $_POST["message"]; ?>
+</body>
+</html>
